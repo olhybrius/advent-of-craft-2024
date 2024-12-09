1c11ccd19a6602975e3ed77e5e72bfb4bcec43ca
olivier <olivier.thiel@protonmail.com>
2024-12-09 14:53:22 +0100

    refactor: extract constant for EID length

--- exercise/java/day05/src/main/java/eid/EID.java
+++ exercise/java/day05/src/main/java/eid/EID.java
@@ -1,12 +1,15 @@
 package eid;
 
 public class EID {
+
+    private static final int EID_LENGTH = 8;
+
     public EID(String value) {
         if (value == null) {
             throw new IllegalArgumentException("EID cannot be null.");
         }
-        if (value.length() != 8) {
-            throw new IllegalArgumentException("EID must be 8 characters long.");
+        if (value.length() != EID_LENGTH) {
+            throw new IllegalArgumentException("EID must be " + EID_LENGTH + " characters long.");
         }
     }
 }
