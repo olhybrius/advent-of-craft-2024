17829ee2b23d094ae453eebb03b37b359932dae7
olivier <olivier.thiel@protonmail.com>
2024-12-09 15:20:26 +0100

    test: rename some tests

--- exercise/java/day05/src/test/java/eid/EIDTests.java
+++ exercise/java/day05/src/test/java/eid/EIDTests.java
@@ -10,21 +10,21 @@ import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
 class EIDTests {
     @Test
-    void an_EID_should_not_be_null() {
+    void an_EID_cannot_be_null() {
         thenThrownBy(() -> new EID(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("EID cannot be blank.");
     }
 
     @Test
-    void an_EID_should_not_be_empty() {
+    void an_EID_cannot_be_empty() {
         thenThrownBy(() -> new EID(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("EID cannot be blank.");
     }
 
     @Test
-    void an_EID_should_be_eight_characters_long() {
+    void an_EID_must_be_eight_characters_long() {
         thenThrownBy(() -> new EID("123456789"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("EID must be 8 characters long.");
