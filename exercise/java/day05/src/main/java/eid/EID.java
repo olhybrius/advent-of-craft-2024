d89d1801c378d8c7ff1aaaaa4eb20374df832744
olivier <olivier.thiel@protonmail.com>
2024-12-09 15:43:59 +0100

    refactor: extract methods

--- exercise/java/day05/src/main/java/eid/EID.java
+++ exercise/java/day05/src/main/java/eid/EID.java
@@ -32,6 +32,31 @@ public class EID {
     private static final int CONTROL_KEY_START_INDEX = 6;
 
     public EID(String value) {
+        validateFormat(value);
+        validateLength(value);
+        validateSexIdentifier(value);
+        validateControlKey(value);
+    }
+
+    private static void validateSexIdentifier(String value) {
+        ElfSex.getByIdentifier(value.charAt(0));
+    }
+
+    private void validateControlKey(String value) {
+        var number = Integer.parseInt(value.substring(0, CONTROL_KEY_START_INDEX));
+        var controlKey = Integer.parseInt(value.substring(CONTROL_KEY_START_INDEX, EID_LENGTH));
+        if (CONTROL_KEY_MAX_VALUE - number % CONTROL_KEY_MAX_VALUE != controlKey) {
+            throw new IllegalArgumentException("Invalid control key.");
+        }
+    }
+
+    private void validateLength(String value) {
+        if (value.length() != EID_LENGTH) {
+            throw new IllegalArgumentException("EID must be " + EID_LENGTH + " characters long.");
+        }
+    }
+
+    private void validateFormat(String value) {
         if (StringUtils.isBlank(value)) {
             throw new IllegalArgumentException("EID cannot be blank.");
         }
@@ -39,15 +64,6 @@ public class EID {
             Integer.parseInt(value);
         } catch(NumberFormatException numberFormatException) {
             throw new IllegalArgumentException("EID must only contain digits.");
-        }
-        if (value.length() != EID_LENGTH) {
-            throw new IllegalArgumentException("EID must be " + EID_LENGTH + " characters long.");
-        }
-        ElfSex.getByIdentifier(value.charAt(0));
-        var number = Integer.parseInt(value.substring(0, CONTROL_KEY_START_INDEX));
-        var controlKey = Integer.parseInt(value.substring(CONTROL_KEY_START_INDEX, EID_LENGTH));
-        if (CONTROL_KEY_MAX_VALUE - number % CONTROL_KEY_MAX_VALUE != controlKey) {
-            throw new IllegalArgumentException("Invalid control key.");
         }
     }
 }
